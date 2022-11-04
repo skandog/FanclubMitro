64d27ae2d561577c8ce4914a5296e26980711c1c
skandog <muumrar@gmail.com>
2022-11-04 15:43:12 +0000

    added settings to twett file

--- tweet.php
+++ tweet.php
@@ -1,3 +1,11 @@
 <?php
 require_once('config.php');
 require_once('TwitterAPIExchange.php');
+
+// settings for twitter api connection
+$settings = array(
+    'oauth_access_token' => TWITTER_ACCESS_TOKEN,
+    'oauth_access_token_secret' => TWITTER_ACCESS_TOKEN_SECRET,
+    'consumer_key' => TWITTER_CONSUMER_KEY,
+    'consumer_secret' => TWITTER_CONSUMER_SECRET
+);
