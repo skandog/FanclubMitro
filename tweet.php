8ca35212a356bb8f2352262871891ad819332d6d
skandog <muumrar@gmail.com>
2022-11-04 17:28:56 +0000

    confirmed connection to the api to search tweets, but not post, getting 401. enough for today. might move to ANOTHER pack to get this working lol

--- tweet.php
+++ tweet.php
@@ -11,15 +11,36 @@ use Abraham\TwitterOAuth\TwitterOAuth;
 
 // settings for twitter api connection
 $settings = array(
-    'oauth_access_token' => TWITTER_ACCESS_TOKEN,
-    'oauth_access_token_secret' => TWITTER_ACCESS_TOKEN_SECRET,
-    'consumer_key' => TWITTER_CONSUMER_KEY,
-    'consumer_secret' => TWITTER_CONSUMER_SECRET
+    'oauth_access_token' => ACCESS_TOKEN,
+    'oauth_access_token_secret' => ACCESS_TOKEN_SECRET,
+    'consumer_key' => CONSUMER_KEY,
+    'consumer_secret' => CONSUMER_SECRET
 );
 
-$connection = new TwitterOAuth(TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET);
+$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+
+$status = 'Testing, testing. Any #mitro fans out there?';
+
+// $result = $connection->get('search/tweets', ['q' => 'hello']);
 
 
+$result = $connection->post("statuses/update", ["status" => $status]);
+// try {
+//     //actual code
+//     echo "tweeeeeting";
+// } catch (Exception $e) {
+//     echo $e->getMessage();
+// }
+
+
+
+if ($connection->getLastHttpCode() == 200) {
+    echo "Your Tweet posted successfully.";
+    var_dump($result);
+} else {
+    echo $connection->getLastHttpCode();
+    echo 'error: ' . $result->errors[0]->message;
+}
 
 // twitter api endpoint
 $url = 'https://api.twitter.com/2/tweets';
@@ -32,15 +53,3 @@ $apiData = array(
     'content-type' => 'application/json',
     'status' => 'This tweet is comming from an awesome script written using php and the Twitter API! #Geek #PHP #TwitterAPI',
 );
-
-
-// create new twitter for api communication
-$twitter = new TwitterAPIExchange($settings);
-
-// make our api call to twiiter
-$twitter->buildOauth($url, $requestMethod);
-$twitter->setPostfields($apiData);
-$response = $twitter->performRequest(true, array(CURLOPT_SSL_VERIFYHOST => 0, CURLOPT_SSL_VERIFYPEER => 0));
-
-echo '<pre>';
-print_r(json_decode($response, true));
