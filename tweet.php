28500176c768e81e079d542d942f18de08549c15
skandog <muumrar@gmail.com>
2022-11-04 15:50:01 +0000

    written code to connect to endpoint, and defined tweet object

--- tweet.php
+++ tweet.php
@@ -9,3 +9,23 @@ $settings = array(
     'consumer_key' => TWITTER_CONSUMER_KEY,
     'consumer_secret' => TWITTER_CONSUMER_SECRET
 );
+
+// twitter api endpoint
+$url = 'https://api.twitter.com/1.1/statuses/update.json';
+
+// twitter api endpoint request type
+$requestMethod = 'POST';
+
+// twitter api endpoint data
+$apiData = array(
+    'status' => 'This tweet is comming from an awesome script written using php and the Twitter API! #Geek #PHP #TwitterAPI',
+);
+
+
+// create new twitter for api communication
+$twitter = new TwitterAPIExchange($settings);
+
+// make our api call to twiiter
+$twitter->buildOauth($url, $requestMethod);
+$twitter->setPostfields($apiData);
+$response = $twitter->performRequest(true, array(CURLOPT_SSL_VERIFYHOST => 0, CURLOPT_SSL_VERIFYPEER => 0));
