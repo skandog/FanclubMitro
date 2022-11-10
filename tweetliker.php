6b1bbf41c291e631a7509991c4e3557149287459
skandog <muumrar@gmail.com>
2022-11-10 13:28:37 +0000

    playing with yml still

--- tweetliker.php
+++ tweetliker.php
@@ -13,7 +13,7 @@ echo 'My username is ' . getenv("ACCESS_TOKEN") . '!';
 
 // settings for twitter api connection
 $settings = array(
-    'oauth_access_token' => ACCESS_TOKEN,
+    'oauth_access_token' => getenv("ACCESS_TOKEN"),
     'oauth_access_token_secret' => ACCESS_TOKEN_SECRET,
     'consumer_key' => CONSUMER_KEY,
     'consumer_secret' => CONSUMER_SECRET
