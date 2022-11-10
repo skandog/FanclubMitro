5801e75ef84b77f76e40eb9da6e9cbcf23999323
skandog <muumrar@gmail.com>
2022-11-10 15:35:30 +0000

    added more facts about mitro :)

--- assets/mitrofacts.php
+++ assets/mitrofacts.php
@@ -4,10 +4,19 @@ $facts = [
     "Aleksandar Mitrović is a Serbian professional footballer who plays as a striker for Premier League club Fulham and the England.",
     "He won the Serbian SuperLiga in his first season with .@FKPartizanBG, his boyhood club",
     "After dazzling at Partizan, he then joined .@rscanderlecht for a club record €5 million.",
+    "Mitrović helped Serbia win the 2013 European Under-19 Championship, being voted the best player of the tournament.",
+    "That same year 2013, he played his first game for the senior national team",
+    "Aleksandar Mitrović is Serbia's highest goalscorer with 50 and counting.",
     "At .@rscanderlecht he scored 44 goals in 90 games across all competitions in his two-season spell at the club.",
     "He won the Belgian Pro League in his first campaign at .@rscanderlecht, and was the league's top scorer in his second.",
     "Mitrović won two different leagues in both of his debut seasons! How mad is that, talisman 🧿",
     "In 2015, Mitro moved to .@NUFC United for £13 million, amid pletny of fanfare for the talented youngster.",
     "It was a shame for Aleksander that this move did not work out as he was, and remains, a .@NUFC fan",
-    "Mitro was loaned to .@FulhamFC in 2018, and the fans were delighted when he signed permanently after helping them achieve promotion to the prem."
+    "Mitro was loaned to .@FulhamFC in 2018, and the fans were delighted when he signed permanently after helping them achieve promotion to the prem.",
+    "We love Mitro he is such a legend!",
+    "Did you know: Mitro was born in Stoke??? I didn't!",
+    "It took Mitro only 9 mins to score his first .@ChampionsLeague goal, aged 18. On his official .@FKPartizanBG debut no less!",
+    "By the end of his debut season, Mitrović was Partizan's top scorer in all competitions with 15 goals in 36 appearances even though he was one of the youngest players of the team.",
+    "Did you know Mitro had a shift between the sticks? On 10 December 2013, in the last round of the .@ChampionsLeague Group stage against Olympiacos, Mitrović replaced goalkeeper Silvio Proto who had been sent off.",
+    "On 5 November 2014, in the .@ChampionsLeague Group D game against Arsenal, he scored the equaliser in the 90th minute, completing .@rscanderlecht 's comeback from 3–0 to 3–3."
 ];
