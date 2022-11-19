5225041b68702da6e2a6f2920d969ac2f2ed9bac
skandog <muumrar@gmail.com>
2022-11-19 13:05:08 +0000

    created new asset for serbian facts ahead of theworld cup

---
+++ assets/mitroserbiafacts.php
@@ -0,0 +1,15 @@
+<?php
+
+$serbfacts = [
+    "Four goals saw Mitrović top scorer of the Serbian national under-19 team in their successful qualifying campaign for the 2012 UEFA European Under-19 Championship.",
+    "On 26 March 2013, Mitrović scored two goals in a friendly for Serbia's U21 team against Bulgaria U21",
+    "Mitrović was called up by Serbia's U19 coach Ljubinko Drulović for two qualification matches for the 2013 UEFA European Under-19 Championship taking place after his debut with the senior national side against Belgium .@BelRedDevils.",
+    "Mitrović was also a member of the squad that travelled to Lithuania for the final tournament, where he established himself as one of the key players in Serbia's U19 team which won the competition for the first time in the history of Serbian football. Born winner.",
+    "He contributed by scoring a goal and providing two assists during the contest (including one in the final match against France U19)",
+    "Mitrović was also named the European Under-19 Championship tournament's Golden Player. Shining.",
+    "Mitrović earned his first call-up to the Serbian senior national team by coach Siniša Mihajlović for a 2014 FIFA World Cup qualifier against Belgium. He played 69 minutes on his debut on 7 June 2013 and earned a yellow card after stepping on Axel Witsel's foot. #WorldCup #debut",
+    "On 6 September 2013, Mitrović scored his first goal for the senior side in a 1–1 home draw against Croatia in another World Cup qualifier. #WorldCup",
+    "In the 2018 FIFA World Cup qualifiers, Mitrović scored three goals in two matches to ensure Serbia were still in contention to qualify from their group."
+
+
+];
